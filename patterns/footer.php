161f8749c81e42c6c4c5c0318f8de6bfc196bae5
Martin Clément <martin.clement@outlook.com>
2023-03-21 18:47:05 +0100

    Modify header and footer

---
+++ patterns/footer.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Title: footer
+ * Slug: lensmarktheme/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"left","id":197,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image alignleft size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/gantrisch_x_bfh.png" alt="" class="wp-image-197"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>Eine Zusammenarbeit zwischen Förderverein Region Gantrisch und der Berner Fachhochschule</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"right","id":198,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image alignright size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/schweizer_praerke.png" alt="" class="wp-image-198"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:separator {"align":"wide","className":"is-style-wide"} -->
+<hr class="wp-block-separator alignwide has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:columns {"verticalAlignment":"top"} -->
+<div class="wp-block-columns are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:paragraph -->
+<p>Schlossgasse 13<br>3150 Schwarzenburg<br>031 808 00 20<br>info@gantrisch.ch</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:paragraph -->
+<p>Montag – Freitag<br>08.00 – 12.00 Uhr<br>13.30 – 17.00 Uhr</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right","verticalAlignment":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Zur Hauptseite (gantrisch.ch)</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph -->
+<p>© Naturpark Gantrisch 2023</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:navigation {"ref":163} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
